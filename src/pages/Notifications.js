76b29034c17a893e8e1fd6cfc01810e9a3f622f2
gage <gvankuilenburg@mail.valenciacollege.edu>
2023-01-18 22:11:18 +0000

    Update Notifications.js

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -2,26 +2,85 @@ import Deso from "deso-protocol";
 import { useRecoilValue } from "recoil";
 import { PublicKey } from "../State/App.state";
 import { useEffect, useState } from "react";
+import {
+  Avatar,
+  Paper,
+  Group,
+  Text,
+  Space,
+  Center,
+  Divider,
+} from "@mantine/core";
 const deso = new Deso();
 
 export default function Notifications() {
   const [notifications, setNotifications] = useState([]);
+  const [profilePics, setProfilePics] = useState("");
+  const [userName, setUsername] = useState("");
   const publicKey = useRecoilValue(PublicKey);
   useEffect(() => {
     getNotifications();
-  });
+  }, []);
 
   const getNotifications = async () => {
     const request = {
       NumToFetch: 50,
       PublicKeyBase58Check: publicKey,
-      FetchStartIndex: 100,
+      FetchStartIndex: 1000,
     };
+    const response = await deso.notification.getNotifications(request);
 
-    const response = await deso.notifications.getNotifications(request);
-    setNotifications(response);
-    console.log(response);
+    const profilePics = {};
+
+    response.data.Notifications.forEach((notification) => {
+      profilePics[
+        notification.Metadata.AffectedPublicKeys[0].PublicKeyBase58Check
+      ] = deso.user.getSingleProfilePicture(
+        notification.Metadata.AffectedPublicKeys[0].PublicKeyBase58Check
+      );
+    });
+
+    setNotifications(response.data.Notifications);
+    setProfilePics(profilePics);
   };
 
-  return <div>?</div>;
+  return (
+    <div>
+      <Divider
+        my="xs"
+        label={
+          <>
+            <Text fw={444} fz="xl">
+              Notifications
+            </Text>
+          </>
+        }
+        labelPosition="center"
+      />
+      {notifications.map((notification, index) => (
+        <div key={index}>
+          <Space h="sm" />
+          <Center>
+            <Paper shadow="lg" p="md" withBorder>
+              <Group>
+                <Avatar
+                  size={33}
+                  radius={33}
+                  src={
+                    profilePics[
+                      notification.Metadata.AffectedPublicKeys[0]
+                        .PublicKeyBase58Check
+                    ]
+                  }
+                />
+
+                <Text>{notification.Metadata.TxnType}</Text>
+              </Group>
+            </Paper>
+          </Center>
+          <Space h="sm" />
+        </div>
+      ))}
+    </div>
+  );
 }
