71bcb7b6efeb6bb6f525b9cbf139d5b3644cf9fc
gage <gvankuilenburg@mail.valenciacollege.edu>
2023-01-18 23:02:40 +0000

    Update Discover.js

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,3 +1,98 @@
+import { Grid, Paper, Center, Divider, Text } from "@mantine/core";
+import { TwitchPlayer } from "react-twitch-embed";
+import { useRef } from "react";
+
 export default function Discover() {
-  return <div>Discover</div>;
+  const embed = useRef(); // We use a ref instead of state to avoid rerenders.
+
+  const handleReady = (e) => {
+    embed.current = e;
+  };
+  return (
+    <>
+      <Divider
+        my="xs"
+        label={
+          <>
+            <Text fw={444} fz="xl">
+              Discover
+            </Text>
+          </>
+        }
+        labelPosition="center"
+      />
+      <Grid grow>
+        <Grid.Col span={4}>
+          <Center>
+            <Paper shadow="xl" radius="md" p="xs">
+              <TwitchPlayer
+                channel="shanks_ttv"
+                autoplay
+                muted
+                width={333}
+                onReady={handleReady}
+                id="1"
+              />
+            </Paper>
+          </Center>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Center>
+            <Paper shadow="xl" radius="md" p="xs">
+              <TwitchPlayer
+                channel="GMHikaru"
+                autoplay
+                muted
+                width={333}
+                onReady={handleReady}
+                id="2"
+              />
+            </Paper>
+          </Center>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Center>
+            <Paper shadow="xl" radius="md" p="xs">
+              <TwitchPlayer
+                channel="sgares"
+                autoplay
+                muted
+                width={333}
+                onReady={handleReady}
+                id="3"
+              />
+            </Paper>
+          </Center>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Center>
+            <Paper shadow="xl" radius="md" p="xs">
+              <TwitchPlayer
+                channel="shroud"
+                autoplay
+                muted
+                width={333}
+                onReady={handleReady}
+                id="4"
+              />
+            </Paper>
+          </Center>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Center>
+            <Paper shadow="xl" radius="md" p="xs">
+              <TwitchPlayer
+                channel="subroza"
+                autoplay
+                muted
+                width={333}
+                onReady={handleReady}
+                id="5"
+              />
+            </Paper>
+          </Center>
+        </Grid.Col>
+      </Grid>
+    </>
+  );
 }
